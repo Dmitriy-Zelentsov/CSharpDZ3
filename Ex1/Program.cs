c08a38d63850c7770fda82ef8f098ea594575c97
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-12 15:28:55 +0300

    add Ex2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,22 +2,3 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-void FindPalindrop (int p)
-{
-    string a = p.ToString();
-    Console.WriteLine(a);
-    char[] b = a.ToCharArray();
-    char[] revers_b = a.Reverse().ToArray();
-    string c =  String.Concat<char>(revers_b);
-    Console.WriteLine(c);
-
-    if(a == c)
-    {
-        Console.WriteLine("Да");
-    }
-    else
-    {
-        Console.WriteLine("Нет");
-    }
-}
-    FindPalindrop(12321);
