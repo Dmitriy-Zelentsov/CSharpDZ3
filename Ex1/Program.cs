20e5b4f67fde1d05f63eeaf9e1a156ac6870202f
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 11:58:13 +0300

    Последние измениения

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,3 +2,22 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
+void FindPalindrom (int p)
+{
+    string a = p.ToString();
+    Console.WriteLine($"{a} - исходное число");
+    char[] b = a.ToCharArray();
+    char[] revers_b = a.Reverse().ToArray();
+    string c =  String.Concat<char>(revers_b);
+    Console.WriteLine($"{c} - число реверс");
+
+    if(a == c)
+    {
+        Console.WriteLine("Да, палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+    FindPalindrom(12321);
